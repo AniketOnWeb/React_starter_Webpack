8f3723752779024189e0d5616f7f4d296f5cd959
Aniket Chaudhary <chaudharyaniket16@gmail.com>
2020-01-26 22:37:52 +0530

    add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { App } from './App';
+import React from "react";
+import { render } from "react-dom";
+import ReactDOM from "react-dom";
+import { App } from "./App";
 
-
-render(<App />, document.getElementById('root'));
+const wrapper = document.getElementById("root");
+wrapper ? ReactDOM.render(<App />, wrapper) : false;
